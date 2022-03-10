96b7596bbd5ea85e32e096aea86f798d4f4f0504
Roshan Paturkar <paturkarr@gmail.com>
2022-03-11 01:38:05 +0530

    Home page, coming soon!

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,286 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:move_to_background/move_to_background.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String id = 'login_screen';
+
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _isHidden = true;
+  bool startBouncer = false;
+  final emailFieldController = TextEditingController();
+  final passwordFieldController = TextEditingController();
+
+  var email = 'paturkarr@gmail.com';
+  var password = 'redhat';
+
+  SnackBar showSnackBar(var message) {
+    return SnackBar(content: Text(message));
+  }
+
+  void _togglePasswordView() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (Platform.isAndroid || Platform.isIOS) {
+          MoveToBackground.moveTaskToBack();
+        }
+        return false;
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xFF0181A20),
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Welcome Back!',
+                    style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  const Text(
+                    'Please sign in to your account',
+                    style: TextStyle(
+                      color: Color(0xFF6F7075),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 66,
+                  ),
+                  Container(
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF262A34),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                      child: TextField(
+                        controller: emailFieldController,
+                        cursorColor: Colors.white,
+                        obscureText: false,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          // icon: Icon(
+                          //   Icons.mail_outline,
+                          //   color: Color(0xFF6F7075),
+                          // ),
+                          border: InputBorder.none,
+                          hintText: 'Email',
+                          hintStyle: TextStyle(
+                            color: Color(0xFF6F7075),
+                          ),
+                          // errorText: password == null || password == ''
+                          //     ? 'Please enter something'
+                          //     : null,
+                          // border: OutlineInputBorder(
+                          //   borderRadius: BorderRadius.circular(20),
+                          // ),
+                        ),
+                        style: const TextStyle(
+                          color: Color(0xFFD5D6D9),
+                        ),
+                        onChanged: (value) {
+                          email = value;
+                          print(email);
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  Container(
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF262A34),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+                      child: TextField(
+                        controller: passwordFieldController,
+                        cursorColor: Colors.white,
+                        obscureText: _isHidden,
+                        keyboardType: TextInputType.visiblePassword,
+                        decoration: InputDecoration(
+                          // icon: Icon(
+                          //   Icons.vpn_key_outlined,
+                          //   color: Color(0xFF6F7075),
+                          // ),
+                          border: InputBorder.none,
+                          hintText: 'Password',
+                          hintStyle: const TextStyle(
+                            color: Color(0xFF6F7075),
+                          ),
+                          suffix: InkWell(
+                            onTap: _togglePasswordView,
+                            child: Icon(
+                              _isHidden
+                                  ? Icons.visibility_outlined
+                                  : Icons.visibility_off_outlined,
+                              color: Color(0xFF6A6B71),
+                            ),
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Color(0xFFD5D6D9),
+                        ),
+                        onChanged: (value) {
+                          password = value;
+                          print(password);
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigator.pushNamed(context, ForgetPassword.id);
+                    },
+                    child: Container(
+                      width: 350,
+                      child: const Text(
+                        'Forget Password?',
+                        textAlign: TextAlign.right,
+                        style: TextStyle(
+                          color: Color(0xFF6F7075),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Container(
+                    width: 350,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF5468FF),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              startBouncer = true;
+                            });
+                            // signInWithEmailAndPassword(context);
+                            //Navigator.pushNamed(context, DashboardScreen.id);
+                          },
+                          child: const Text(
+                            'Sign In',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        startBouncer
+                            ? const SpinKitDoubleBounce(
+                                color: Colors.white,
+                                size: 50.0,
+                              )
+                            : const SizedBox(),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  Container(
+                    width: 350,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'images/google.png',
+                          height: 26,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              showSnackBar(
+                                  'This feature is currently unavailable!'),
+                            );
+                          },
+                          child: const Text(
+                            'Sign In with Google',
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigator.pushNamed(context, RegisterScreen.id);
+                    },
+                    child: Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Dont\'t have an Account?',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 4,
+                          ),
+                          Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Color(0xFF5468FF),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartScreen extends StatefulWidget {
+  static const String id = 'start_screen';
+
+  const StartScreen({Key? key}) : super(key: key);
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  var start = 'Get Started';
+  bool startBouncer = false;
+
+  SnackBar showSnackBar(var message) {
+    return SnackBar(content: Text(message));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'images/broomsticks.jpg',
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    text: 'The Three ',
+                    style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                    children: const <TextSpan>[
+                      TextSpan(
+                        text: "Broomsticks",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: " !",
+                        style: TextStyle(
+                          color: Color(0xFF5468FF),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'We\'re only as strong as we are united, ',
+                  style: GoogleFonts.ubuntu(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'and as weak as we are divided.',
+                  style: GoogleFonts.ubuntu(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                // Text(
+                //   'and use one another, even relate to',
+                //   style: GoogleFonts.ubuntu(
+                //     fontWeight: FontWeight.bold,
+                //     color: Colors.white,
+                //   ),
+                // ),
+                // Text(
+                //   'one another. A warm, messy',
+                //   style: GoogleFonts.ubuntu(
+                //     fontWeight: FontWeight.bold,
+                //     color: Colors.white,
+                //   ),
+                // ),
+                // Text(
+                //   'circle of humanity.',
+                //   style: GoogleFonts.ubuntu(
+                //     fontWeight: FontWeight.bold,
+                //     color: Colors.white,
+                //   ),
+                // ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 350,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF5468FF),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () async {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            showSnackBar('Coming soon....!'),
+                          );
+
+                          // Navigator.pushNamed(context, LoginScreen.id);
+                        },
+                        child: Text(
+                          start,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      startBouncer
+                          ? const SpinKitDoubleBounce(
+                              color: Colors.white,
+                              size: 50.0,
+                            )
+                          : const SizedBox(),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//'We're only as strong as we are united, and as weak as we are divided.'
+//'There are some things you can't share without ending up liking each other, and knocking out a twelve-foot mountain troll is one them'
