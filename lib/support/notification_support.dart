368c0a25e90c31575049e3296fa527c7b275ecd1
Roshan Paturkar <paturkarr@gmail.com>
2022-04-27 01:40:18 +0530

    D3Broomsticks 1.1 release
    1. Notification integrated for WEB and Android
    2. Icons and Name changed

--- lib/support/notification_support.dart
+++ lib/support/notification_support.dart
@@ -1,10 +1,14 @@
 import 'dart:convert';
 
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class NotificationSupport {
+  var baseUrl = GetPlatform.isWeb
+      ? 'https://d3broomsticks-web-ns.deta.dev'
+      : 'https://d3broomsticks-android-ns-270422.deta.dev';
   void tokenRefresher(var data) async {
-    var url = 'https://d3broomsticks-android-ns.deta.dev/v1/api/user/token';
+    var url = '$baseUrl/v1/api/user/token';
 
     String body = json.encode(data);
     try {
@@ -17,8 +21,7 @@ class NotificationSupport {
   }
 
   void sendNotification(var data) async {
-    var url =
-        'https://d3broomsticks-android-ns.deta.dev/v1/api/notification/tokens';
+    var url = '$baseUrl/v1/api/notification/tokens';
 
     String body = json.encode(data);
     try {
